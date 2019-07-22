2d8f0cdde24def5dbe7dc328d9895cab62b7bb0c
Marcio Bernardes <marciobernardes@live.com>
2019-07-22 20:18:59 -0300

    Adicionado router

--- image/urls.py
+++ image/urls.py
@@ -14,8 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from apps.app_resizer.views import ImageViewSet
+
+router = DefaultRouter()
+router.register('image', ImageViewSet, base_name='images')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(router.urls))
 ]
